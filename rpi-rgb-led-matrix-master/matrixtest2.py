8dbb2a20facfe4a67688a973fc2050fe272f5dee
Stina Wargert <stinawargert@Stinas-MacBook-Pro.local>
2017-05-07 15:08:00 +0200

    small fixures

--- rpi-rgb-led-matrix-master/matrixtest2.py
+++ rpi-rgb-led-matrix-master/matrixtest2.py
@@ -46,7 +46,7 @@ for n in range(32, -image.size[0], -1): # Scroll R to L
 # The adafruit.png image has a couple columns of black pixels at
 # the right edge, so erasing after the scrolled image isn't necessary.
 matrix.Clear()
-image = Image.open("hamp.png")
+image = Image.open("graglas.tiff")
 size = 128, 64
 image.thumbnail(size, Image.ANTIALIAS)
 image.load()
