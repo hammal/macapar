6e679b24def80c0fad1279294d824773ad749213
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2017-05-13 14:51:08 +0200

    small fixes

--- rpi-rgb-led-matrix-master/textscript.py
+++ rpi-rgb-led-matrix-master/textscript.py
@@ -1,4 +1,5 @@
 import time
-text = "Halloj detta ar mitt jatte langa meddelande"
-while True:
-   print(text)
+text = ["En elefant balanserade", "På en liten spindeltråd", "Det tyckte han var så intressant,", "Så han gick och hämtade en annan elefant."]
+while text:
+   print(text.pop(0))
+   time.sleep(5)
