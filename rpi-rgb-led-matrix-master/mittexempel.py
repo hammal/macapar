16311e223f2f0d69dd35b0f9f87ae0060bc40867
Stina Wargert <stinawargert@Stinas-MacBook-Pro.local>
2017-05-13 14:31:50 +0200

    bajs

--- rpi-rgb-led-matrix-master/mittexempel.py
+++ rpi-rgb-led-matrix-master/mittexempel.py
@@ -45,6 +45,7 @@ for n in range(32, -image.size[0], -1): # Scroll R to L
 	matrix.SetImage(image.im.id, n, 0)
 	time.sleep(0.025)
 
+print("hej")
 # # 24-bit RGB scrolling example.
 # # The adafruit.png image has a couple columns of black pixels at
 # # the right edge, so erasing after the scrolled image isn't necessary.
