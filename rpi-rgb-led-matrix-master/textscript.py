4b8239a8be3a0204c81e0f217c7a0b01ccb4f0f5
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2017-05-13 14:58:37 +0200

    small fixes

--- rpi-rgb-led-matrix-master/textscript.py
+++ rpi-rgb-led-matrix-master/textscript.py
@@ -1,5 +1,6 @@
 import time
 text = ["En elefant balanserade", "Pa en liten spindeltrad", "Det tyckte han var sa intressant,", "Sa han gick och hamtade en annan elefant."]
 while text:
-   print(text.pop(0))
-   time.sleep(5)
+    print("")
+    print(text.pop(0),)
+    time.sleep(5)
