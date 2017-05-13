390dd3ab1394760fb77002b27f16ed2310fd225c
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2017-05-13 15:02:42 +0200

    added sys.write

--- rpi-rgb-led-matrix-master/textscript.py
+++ rpi-rgb-led-matrix-master/textscript.py
@@ -2,7 +2,7 @@ import time
 import sys
 text = ["En elefant balanserade", "Pa en liten spindeltrad", "Det tyckte han var sa intressant,", "Sa han gick och hamtade en annan elefant."]
 while text:
+    sys.stdout.write("\n")
     sys.stdout.write(text.pop(0))
     sys.stdout.flush()
     time.sleep(5)
-    sys.stdout.write("\n")
