e938479b7831f3972433c39ea28a5332f3873ca5
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2017-06-03 17:27:58 +0200

    Fixed error with pathmaker

--- rpi-rgb-led-matrix-master/makeEmojiPickleList.py
+++ rpi-rgb-led-matrix-master/makeEmojiPickleList.py
@@ -5,8 +5,7 @@ emojipaths = []
 
 for root, dirs, files in os.walk("./emojis/", topdown=False):
     for name in files:
-        emojipaths.append(os.path.abspath(name))
-
+        emojipaths.append(os.path.abspath(os.path.join("./emojis/",name)))
 
 # save as pickle
 with open("emojilist.txt", "wb") as fp:   #Pickling
