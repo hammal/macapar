34710e2ead64ef20c7fbd26ef8bf898d6942d33f
Hampus Malmberg <malmberg@isi.ee.ethz.ch>
2017-06-03 18:44:09 +0200

    Added form

--- rpi-rgb-led-matrix-master/makeEmojiPickleList.py
+++ rpi-rgb-led-matrix-master/makeEmojiPickleList.py
@@ -1,11 +1,13 @@
 import os
 import pickle
 
-emojipaths = []
+emojipaths = {}
 
 for root, dirs, files in os.walk("./emojis/", topdown=False):
+    for dir in dirs:
+        emojipaths[dir] = []
     for name in files:
-        emojipaths.append(os.path.abspath(os.path.join("./emojis/",name)))
+        emojipaths[os.path.dirname(name)].append(os.path.abspath(os.path.join("./emojis/",name)))
 
 # save as pickle
 with open("emojilist.txt", "wb") as fp:   #Pickling
--- webinterface/routes/index.js
+++ webinterface/routes/index.js
@@ -11,7 +11,7 @@ var preExecution = "cd ../rpi-rgb-led-matrix-master;";
 var snappsvisa = {
 	name:"Snapsvisa",
   executionName:"Snapsa!",
-	executable : `${preExecution} sudo python snappsvisa1.py`,
+	executable : `sudo python snappsvisa1.py`,
 	description : "Detta är snappsvisan.",
 	api : 'snaps'
 };
@@ -19,7 +19,7 @@ var snappsvisa = {
 var checkStatus = {
 	name:"Status Check",
   executionName:"check it out",
-	executable : `${preExecution} sudo python checkstatus.py`,
+	executable : `sudo python checkstatus.py`,
 	description : "System status check.",
 	api : 'status'
 };
@@ -27,7 +27,7 @@ var checkStatus = {
 var toast = {
 	name:"Toast",
   executionName:"fire in the hole",
-	executable : `${preExecution} sudo python toast.py`,
+	executable : `sudo python toast.py`,
 	description : "Toasting what it is all about.",
 	api : 'toast'
 };
@@ -43,7 +43,7 @@ var intro = {
 var Leif = {
 	name:"Leif",
   executionName:"Leffe",
-	executable : `${preExecution} sudo python intro_Leif.py`,
+	executable : `sudo python intro_Leif.py`,
 	description : "Detta är leifs knapp.",
 	api : 'leif'
 };
@@ -51,9 +51,18 @@ var Leif = {
 var GunillaAnders = {
 	name:"Gunilla och Anders",
   executionName:"Öppna Ridån",
-	executable : `${preExecution} sudo python intro_AoG2.py`,
+	executable : `sudo python intro_AoG2.py`,
 	description : "Gunilla och Anders intro.",
 	api : 'gunillaochanders'
+};
+
+var TextArgument = {
+	name: "Display Text",
+	executionName: "Submit",
+	executable: "sudo python textFranHTML.py",
+	description: "Submit text to display.",
+	api: "textArgument",
+	argument: "Put text here"
 };
 
 var restart = {
@@ -62,7 +71,7 @@ var restart = {
 	executable: "sudo reboot now",
 	description: "This button restarts the RPi for the display",
 	api: "restart"
-}
+};
 
 var shutdown = {
 	name:"Shutdown",
@@ -70,7 +79,7 @@ var shutdown = {
 	executable: "sudo shutdown -h now",
 	description: "This button turns the RPi off safely",
 	api: "shutdown"
-}
+};
 
 var functions = [
 	snappsvisa,
@@ -79,6 +88,7 @@ var functions = [
   intro,
   Leif,
   GunillaAnders,
+	TextArgument,
 	restart,
 	shutdown
 ];
@@ -99,7 +109,7 @@ router.get('/scripts/:name', function(req, res) {
     			console.log('We got the lock!');
     			// do stuff
 					console.log(`Running ${functions[index].name} executable ${functions[index].executable}`)
-      		exec(`${functions[index].executable}`, function callback(error, stdout, stderr){
+      		exec(`${preExecution} ${functions[index].executable} `, function callback(error, stdout, stderr){
         		if (error) {
             	console.error(`exec error: ${error}`);
             	return;
@@ -113,6 +123,33 @@ router.get('/scripts/:name', function(req, res) {
       res.redirect('/')
     }
   }
+});
+
+
+router.post('/scripts/:name', function(req, res) {
+	for(index in functions) {
+		if( functions[index].api == req.params.name){
+			mutex.timedLock(5000, function (error) {
+				if (error) {
+					console.log('Could not get the lock within 5 seconds, so gave up');
+				} else {
+					console.log('We got the lock!');
+					// do stuff
+					console.log(`Running ${functions[index].name} executable ${functions[index].executable} "${req.body.argument}"`)
+					exec(`${preExecution} ${functions[index].executable} "${req.body.argument}"`, function callback(error, stdout, stderr){
+						if (error) {
+							console.error(`exec error: ${error}`);
+							return;
+						}
+						console.log(`stdout: ${stdout}`);
+						// console.log(`stderr: ${stderr}`);
+						mutex.unlock();
+					});
+				};
+			});
+		res.redirect('/')
+		};
+	}
 });
 
 module.exports = router;
