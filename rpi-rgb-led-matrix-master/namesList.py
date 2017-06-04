ffd6b04f4859918374ddb457417375f7bc06ae52
swargert <swargert@gmail.com>
2017-06-04 21:16:59 +0200

    slumpgenerator och namnlista addade

--- rpi-rgb-led-matrix-master/namesList.py
+++ rpi-rgb-led-matrix-master/namesList.py
@@ -1,3 +1,6 @@
+#-*- coding: utf-8 -*-
+from __future__ import unicode_literals
+import PIL
 names = [
     "Stina wargert ",
     "Hampus Malmberg",
@@ -68,5 +71,4 @@ names = [
     "Ida Hjalmarsson",
     "Tove och Levent",
     "Maria Hjalmarsson",
-    "Maria Hjalmarsson"
 ]
